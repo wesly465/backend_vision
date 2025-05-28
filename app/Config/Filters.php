a33fb37be84fedbb258c2aa2d2a29bd39acd3b6a
wesly465 <106130703+wesly465@users.noreply.github.com>
2025-05-28 12:32:23 -0400

    Update Filters.php

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -29,7 +29,7 @@ class Filters extends BaseFilters
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
-        
+        'cors'          => Cors::class,
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
